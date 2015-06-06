0ff5d54d63c9669806d68eaa60f65ce4be2c9301
Edgar Ortega <edgarortegaramirez@gmail.com>
2015-06-06 13:13:39 -0500

    Create, List and Delete teams

--- lib/lita/handlers/team.rb
+++ lib/lita/handlers/team.rb
@@ -1,42 +1,62 @@
 module Lita
-  Team = Struct.new(:name)
+  Team = Struct.new(:name) do
+    def key
+      "team:#{name}"
+    end
+
+    def display_name
+      "#{name} team"
+    end
+  end
 
   module Handlers
     class Team < Handler
 
-      route(/create (\S*) team/i, :create, command: true, help: {
+      route(/create (\S*) team/i, :create_team, command: true, help: {
         "create <name> team" => "create team called <name>"
       })
+      route(/(delete|remove) (\S*) team/i, :delete_team, command: true, help: {
+        "(delete|remove) <name> team" => "delete team called <name>"
+      })
+      route(/list teams/i, :list_teams, command: true, help: {
+        "list teams" => "list all teams"
+      })
 
-      def create(response)
-        require 'pry-byebug'; binding.pry
-        team = Team.new(response.match_data[1])
-        teams = get_teams
-        teams << team
-        team
-      end
-
-      def get_teams
-        redis.set("teams", []) if redis.get("teams").nil?
-        MultiJson.load(redis.get("teams")).map do |team|
-          Team
+      def create_team(response)
+        team = Lita::Team.new(response.match_data[1])
+        if redis.exists(team.key)
+          response.reply "#{team.display_name} already exists"
+        else
+          redis.hset(team.key, :name, team.name)
+          response.reply "#{team.display_name} created, add some people to it"
         end
       end
 
-      def set_teams(teams)
-        data = teams.map(&:to_h)
-        redis.set("teams", data)
+      def delete_team(response)
+        team = Lita::Team.new(response.match_data[2])
+        if redis.exists(team.key)
+          redis.del team.key
+          response.reply "#{team.display_name} deleted"
+        else
+          response.reply "#{team.display_name} does not exist"
+        end
       end
 
-      # route(/^echo\s+(.+)/, :echo, command: true, help: {
-      #   "echo TEXT" => "Replies back with TEXT."
-      # })
-      # def echo(response)
-      #   response.reply(response.matches)
-      # end
+      def list_teams(response)
+        keys = redis.keys "team*"
+        if keys.empty?
+          response.reply "No team has been created so far"
+        else
+          message = "Teams:\n"
+          keys.each do |key|
+            team = redis.hgetall(key)
+            message << "#{team["name"]}\n"
+          end
+          response.reply(message)
+        end
+      end
     end
 
     Lita.register_handler(Team)
-
   end
 end
--- spec/lita/handlers/team_spec.rb
+++ spec/lita/handlers/team_spec.rb
@@ -1,4 +1,54 @@
 require "spec_helper"
 
 describe Lita::Handlers::Team, lita_handler: true do
+  describe "routes" do
+    it { is_expected.to route_command("create testing team").to(:create_team) }
+    it { is_expected.to route_command("delete testing team").to(:delete_team) }
+    it { is_expected.to route_command("list teams").to(:list_teams) }
+  end
+
+  describe "create team" do
+    it "creates a new team" do
+      send_command "create testing team"
+      expect(replies.last).to eq("testing team created, add some people to it")
+    end
+
+    context "team already exists" do
+      it "does not create a new team" do
+        send_command "create testing team"
+        send_command "create testing team"
+        expect(replies.last).to eq("testing team already exists")
+      end
+    end
+  end
+
+  describe "delete team" do
+    it "deletes a team" do
+      send_command "create testing team"
+      send_command "delete testing team"
+      expect(replies.last).to eq("testing team deleted")
+    end
+
+    context "team does not exist" do
+      it "does not delete a team" do
+        send_command "delete testing team"
+        expect(replies.last).to eq("testing team does not exist")
+      end
+    end
+  end
+
+  describe "list teams" do
+    it "list all teams" do
+      send_command "create testing team"
+      send_command "list teams"
+      expect(replies.last).to eq("Teams:\ntesting\n")
+    end
+
+    context "without teams" do
+      it "shows a message" do
+        send_command "list teams"
+        expect(replies.last).to eq("No team has been created so far")
+      end
+    end
+  end
 end
